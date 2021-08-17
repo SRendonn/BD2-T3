cc625fa4e8fc068f6cfc5ca8709645a66a38cfdc
Sebastian Rendon <sebasrg0505@gmail.com>
2021-08-17 01:04:27 -0500

    ref: comments for EstadisticaMongoDB

--- src/main/java/com/db2_t3/models/EstadisticaMongoDB.java
+++ src/main/java/com/db2_t3/models/EstadisticaMongoDB.java
@@ -35,27 +35,57 @@ public class EstadisticaMongoDB {
         this.misventas = misventas;
     }
 
+    /**
+     * Añade la información de ventas de cada departamento a la colección estadísticas de la base de datos de MongoDB.
+     * La estructura final del documento es la siguiente:
+     * {@code
+     * {
+     *     departamento: String,
+     *     misventas: [{
+     *         ciudad: String,
+     *         totalVentas: Int,
+     *         vendedor: {
+     *             cedula: Int,
+     *             ventas: Int
+     *         }
+     *     }]
+     * }
+     * }
+     * @param departamentos Lista de departamentos
+     * */
     public static void addEstadisticas(ArrayList<DepartamentoOracle> departamentos) {
+        // Se realiza la conexión a la base de datos y se selecciona la colección "estadísticas".
         MongoDatabase db = ConexionMongoDB.conectarMongoDB();
         MongoCollection<Document> stats = db.getCollection("estadisticas");
         for(int i = 0; i<departamentos.size(); i++){
+            // Se inicializa el documento que será agregado a la colección con el nombre del departamento.
             Document estadistica = new Document("departamento", departamentos.get(i).getNombre());
             ArrayList<CiudadOracle> cities = departamentos.get(i).getVentasPorCiudad();
+            // Se inicializa la lista misventas, que contendrá la ciudad, el total de ventas y el mejor vendedor.
+            // Esta es una lista de BasicDBObject, una implementación de BSON específica de MongoDB, para Java.
             ArrayList<BasicDBObject> misventas = new ArrayList<>();
             for(int j = 0; j<cities.size(); j++){
+                // Se agrega la ciudad al DBObject
                 BasicDBObject venta = new BasicDBObject("ciudad", cities.get(j).getNombre());
+                // Se agrega el totalVentas al DBObject
                 venta.append("totalVentas", cities.get(j).getTotalVentas());
                 try {
+                    // Se agrega el vendedor como un DBObject compuesto por la cédula y las ventas de este
                     BasicDBObject vendedor = new BasicDBObject("cedula", cities.get(j).getMejorVendedor().getCedula());
                     vendedor.append("ventas", cities.get(j).getMejorVendedor().getVentas());
+                    // Se agrega el vendedor al DBObject
                     venta.append("vendedor", vendedor);
                 }
                 catch (Exception e){
+                    // Se agrega el vendedor al DBObject
                     venta.append("vendedor", null);
                 }
+                // Se agrega el DBObject venta a la lista misventas
                 misventas.add(venta);
             }
+            // Se agrega la lista misventas al documento
             estadistica.append("misventas", misventas);
+            // Se inserta el documento en la base de datos
             InsertOneResult res = stats.insertOne(estadistica);
             System.out.println(res);
         }
