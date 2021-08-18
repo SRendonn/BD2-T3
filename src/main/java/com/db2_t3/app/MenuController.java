3bc68f359999114e1fcc2f061a5650316eb52b56
Salomé Aristizabal <salitomeyo16@gmail.com>
2021-08-17 23:25:17 -0500

    Wip: vaciar arreglos

--- src/main/java/com/db2_t3/app/MenuController.java
+++ src/main/java/com/db2_t3/app/MenuController.java
@@ -1,9 +1,6 @@
 package com.db2_t3.app;
 
-import com.db2_t3.models.CiudadOracle;
-import com.db2_t3.models.ConexionMongoDB;
-import com.db2_t3.models.DepartamentoOracle;
-import com.db2_t3.models.EstadisticaMongoDB;
+import com.db2_t3.models.*;
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -32,6 +29,11 @@ public class MenuController {
 
     public void vaciarArreglos(){
         //Aquí va lo de Salo jeje
+        System.out.println( "Seleccionando..." );
+        ConexionOracle.consultarOracle("CREATE TABLE historicoventas(cc NUMBER(15) PRIMARY KEY, totalacumuladoventas NUMBER(30) NOT NULL)");
+        ConexionOracle.consultarOracle("INSERT INTO historicoventas (cc, totalacumuladoventas) SELECT cedula, total FROM(SELECT e.cc as cedula, SUM(v.nro_unidades*v.miprod.precio_unitario) AS total FROM empleado e, TABLE(e.ventas) v GROUP BY e.cc ORDER BY total DESC)");
+        ConexionOracle.consultarOracle("UPDATE empleado SET ventas=NULL");
+        System.out.println("Consulta finalizada.");
         textoInformativo.setText("Arreglos de ventas vaciados en la DB de Oracle");
     }
 
@@ -40,3 +42,4 @@ public class MenuController {
     }
 
 }
+
