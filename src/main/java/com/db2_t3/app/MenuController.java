bc73a0039dcbb1b404d04eb7b7b42fe81d404e9a
Santiago Alexis Valencia Zapata <svalenciaz@unal.edu.co>
2021-08-18 18:33:25 -0500

    ref: alert for many opened ponters in Oracle Database

--- src/main/java/com/db2_t3/app/MenuController.java
+++ src/main/java/com/db2_t3/app/MenuController.java
@@ -4,6 +4,7 @@ import com.db2_t3.models.*;
 import com.db2_t3.models.DepartamentoOracle;
 import com.db2_t3.models.EstadisticaMongoDB;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
@@ -15,9 +16,17 @@ public class MenuController {
     Text textoInformativo;
 
     public void generarEstadisticas(){
-        ArrayList<DepartamentoOracle> departamentos = DepartamentoOracle.obtenerDepartamentos();
-        EstadisticaMongoDB.addEstadisticas(departamentos, true);
-        textoInformativo.setText("Estadísticas generadas y cargadas en MongoDB");
+        try{
+            ArrayList<DepartamentoOracle> departamentos = DepartamentoOracle.obtenerDepartamentos();
+            EstadisticaMongoDB.addEstadisticas(departamentos, true);
+            textoInformativo.setText("Estadísticas generadas y cargadas en MongoDB");
+        }catch (NullPointerException npe){
+            Alert alert = new Alert(Alert.AlertType.WARNING,
+                    "La conexión a la base de datos de Oracle ha sido estropeada por la conexión de múltiples cursores al mismo tiempo\nPara continuar, cierre estos cursores y reinicie la conexión con la base de datos");
+            alert.show();
+            textoInformativo.setText("Cierra otras conexiones y reinicia la conexión con la base de datos de Oracle para continuar");
+        }
+
     }
 
     public void visualizarEstadisticas() throws IOException {
