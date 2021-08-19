82b5ac2ea362deb62cae2e0cf9919bcab19c62b1
Salomé Aristizabal <salitomeyo16@gmail.com>
2021-08-19 00:38:54 -0500

    fix: boton vaciarArreglos

--- src/main/java/com/db2_t3/app/MenuController.java
+++ src/main/java/com/db2_t3/app/MenuController.java
@@ -7,6 +7,8 @@ import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class MenuController {
@@ -26,16 +28,13 @@ public class MenuController {
 
     public void vaciarArreglos(){
         System.out.println( "Seleccionando..." );
-        if (ConexionOracle.consultarOracle("SELECT * FROM historicoventas") != null){
-            ConexionOracle.consultarOracle("CREATE TABLE aux(cc NUMBER(15) PRIMARY KEY, totalacumuladoventas NUMBER(30), nuevototal NUMBER(30) NOT NULL)");
-            ConexionOracle.consultarOracle("INSERT INTO aux (cc, nuevototal) SELECT cedula, total FROM(SELECT e.cc as cedula, SUM(v.nro_unidades*v.miprod.precio_unitario) AS total FROM empleado e, TABLE(e.ventas) v GROUP BY e.cc ORDER BY total DESC)");
-            ConexionOracle.consultarOracle("UPDATE aux SET totalacumuladoventas = (SELECT totalacumuladoventas FROM historicoventas WHERE historicoventas.cc = aux.cc)");
-            ConexionOracle.consultarOracle("UPDATE historicoventas SET totalacumuladoventas = (SELECT SUM(nuevototal+totalacumuladoventas) FROM aux WHERE historicoventas.cc = aux.cc)");
-            ConexionOracle.consultarOracle("DROP TABLE aux");
-        }
-        else {
-            ConexionOracle.consultarOracle("INSERT INTO historicoventas (cc, totalacumuladoventas) SELECT cedula, total FROM(SELECT e.cc as cedula, SUM(v.nro_unidades*v.miprod.precio_unitario) AS total FROM empleado e, TABLE(e.ventas) v GROUP BY e.cc ORDER BY total DESC)");
-        }
+        ConexionOracle.consultarOracle("CREATE TABLE aux(cc NUMBER(15) PRIMARY KEY, totalacumuladoventas NUMBER(30) NOT NULL, nuevototal NUMBER(30) NOT NULL)");
+        ConexionOracle.consultarOracle("INSERT INTO aux (cc, nuevototal, totalacumuladoventas) SELECT cedula, total, 0 FROM(SELECT e.cc as cedula, SUM(v.nro_unidades*v.miprod.precio_unitario) AS total FROM empleado e, TABLE(e.ventas) v GROUP BY e.cc ORDER BY total DESC)");
+        ConexionOracle.consultarOracle("UPDATE aux SET totalacumuladoventas = (SELECT historicoventas.totalacumuladoventas FROM historicoventas WHERE historicoventas.cc=aux.cc) WHERE EXISTS (SELECT historicoventas.totalacumuladoventas FROM historicoventas WHERE historicoventas.cc = aux.cc)");
+        ConexionOracle.consultarOracle("DELETE FROM historicoventas");
+        ConexionOracle.consultarOracle("INSERT INTO historicoventas (cc, totalacumuladoventas) SELECT cedula, total FROM(SELECT e.cc as cedula, SUM(v.nro_unidades*v.miprod.precio_unitario) AS total FROM empleado e, TABLE(e.ventas) v GROUP BY e.cc ORDER BY total DESC)");
+        ConexionOracle.consultarOracle("UPDATE historicoventas SET totalacumuladoventas = (SELECT SUM(nuevototal+totalacumuladoventas) FROM aux WHERE historicoventas.cc = aux.cc)");
+        ConexionOracle.consultarOracle("DROP TABLE aux");
         ConexionOracle.consultarOracle("UPDATE empleado SET ventas=NULL");
         System.out.println("Consulta finalizada.");
         textoInformativo.setText("Arreglos de ventas vaciados en la DB de Oracle");
