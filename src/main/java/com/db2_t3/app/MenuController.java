0cc3c049d53448969bcec9d42be22b63f451e52a
Salomé Aristizabal <salitomeyo16@gmail.com>
2021-08-18 20:54:59 -0500

    Wip: boton vaciarArreglos reutilizable

--- src/main/java/com/db2_t3/app/MenuController.java
+++ src/main/java/com/db2_t3/app/MenuController.java
@@ -25,10 +25,17 @@ public class MenuController {
     }
 
     public void vaciarArreglos(){
-        //Aquí va lo de Salo jeje
         System.out.println( "Seleccionando..." );
-        ConexionOracle.consultarOracle("CREATE TABLE historicoventas(cc NUMBER(15) PRIMARY KEY, totalacumuladoventas NUMBER(30) NOT NULL)");
-        ConexionOracle.consultarOracle("INSERT INTO historicoventas (cc, totalacumuladoventas) SELECT cedula, total FROM(SELECT e.cc as cedula, SUM(v.nro_unidades*v.miprod.precio_unitario) AS total FROM empleado e, TABLE(e.ventas) v GROUP BY e.cc ORDER BY total DESC)");
+        if (ConexionOracle.consultarOracle("SELECT * FROM historicoventas") != null){
+            ConexionOracle.consultarOracle("CREATE TABLE aux(cc NUMBER(15) PRIMARY KEY, totalacumuladoventas NUMBER(30), nuevototal NUMBER(30) NOT NULL)");
+            ConexionOracle.consultarOracle("INSERT INTO aux (cc, nuevototal) SELECT cedula, total FROM(SELECT e.cc as cedula, SUM(v.nro_unidades*v.miprod.precio_unitario) AS total FROM empleado e, TABLE(e.ventas) v GROUP BY e.cc ORDER BY total DESC)");
+            ConexionOracle.consultarOracle("UPDATE aux SET totalacumuladoventas = (SELECT totalacumuladoventas FROM historicoventas WHERE historicoventas.cc = aux.cc)");
+            ConexionOracle.consultarOracle("UPDATE historicoventas SET totalacumuladoventas = (SELECT SUM(nuevototal+totalacumuladoventas) FROM aux WHERE historicoventas.cc = aux.cc)");
+            ConexionOracle.consultarOracle("DROP TABLE aux");
+        }
+        else {
+            ConexionOracle.consultarOracle("INSERT INTO historicoventas (cc, totalacumuladoventas) SELECT cedula, total FROM(SELECT e.cc as cedula, SUM(v.nro_unidades*v.miprod.precio_unitario) AS total FROM empleado e, TABLE(e.ventas) v GROUP BY e.cc ORDER BY total DESC)");
+        }
         ConexionOracle.consultarOracle("UPDATE empleado SET ventas=NULL");
         System.out.println("Consulta finalizada.");
         textoInformativo.setText("Arreglos de ventas vaciados en la DB de Oracle");
